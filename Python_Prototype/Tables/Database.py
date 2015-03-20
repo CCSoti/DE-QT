0fe4517531eec1b116ac9622dda5842d54d4f23f
Silviya Sotirova <silviya.soti@gmail.com>
2015-03-20 11:14:05 +0000

    Removing some folders

--- Python_Prototype/Tables/Database.py
+++
@@ -1,7 +0,0 @@
-import sys
-import sqlite3
-
-    
-def data():
-    cmd_com = str(sys.argv[1])
-    return cmd_com
--- Python_Prototype/Tables/britice.bat
+++
@@ -1,3 +0,0 @@
-python coordinates.py codemets.db
-python bearing_inclination.py codemets.db
-python core_details.py codemets.db
